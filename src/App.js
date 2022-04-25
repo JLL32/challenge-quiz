cab3f962f7ab04250724ae81bcb1ac7c66cab33b
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-04-25 05:41:27 +0000

    style: replace double quotes with single quotes

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App () {
   return (
     <Center width='100vw' height='100vh'>
       <Stack direction='h' width='400px' height='600px'>
-        <Box padding="10px" height='10%'>
+        <Box padding='10px' height='10%'>
           progress bar
         </Box>
-        <Box padding="10px" height='75%'>
+        <Box padding='10px' height='75%'>
           quiz body
         </Box>
-        <Box padding="10px" height='15%'>
+        <Box padding='10px' height='15%'>
           score bar
         </Box>
       </Stack>
