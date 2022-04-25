d4f26097f06480385a472c1acb57b9e69d99b2b5
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-04-25 05:32:55 +0000

    feat: Add quiz basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Center from './components/Center'
+import Stack from './components/Stack'
+import Box from './components/Box'
 
 function App () {
   return (
-    <div >
-      quiz
-    </div>
+    <Center width='100vw' height='100vh'>
+      <Stack direction='h' width='400px' height='600px'>
+        <Box padding="10px" height='10%'>
+          progress bar
+        </Box>
+        <Box padding="10px" height='75%'>
+          quiz body
+        </Box>
+        <Box padding="10px" height='15%'>
+          score bar
+        </Box>
+      </Stack>
+    </Center>
   )
 }
 
