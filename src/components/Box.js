de62da25e6f42abc9931cb2ba46c8105436ca1ac
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-04-25 05:30:45 +0000

    feat: add a Box container component

---
+++ src/components/Box.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+padding: ${props => props.padding};
+width: ${props => props.width};
+height: ${props => props.height};
+`
+export default Box
