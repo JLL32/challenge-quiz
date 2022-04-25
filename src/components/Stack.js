9d2a1bf110955d99d02d81b09239424f7bd330ab
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-04-25 05:30:28 +0000

    feat: Add a Stack container component

---
+++ src/components/Stack.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Stack = styled.div`
+display: flex;
+flex-direction: ${props => props.direction === 'h' ? 'column' : 'row'};
+padding: ${props => props.padding};
+width: ${props => props.width};
+height: ${props => props.height};
+`
+export default Stack
