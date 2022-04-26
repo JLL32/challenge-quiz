db9f6a84f68ac15ef460b867942c56ec156caabb
JLL32 <eabduljalil@outlook.com>
2022-04-26 19:02:00 +0000

    feat: make choices shuffled

--- src/components/Choices.js
+++ src/components/Choices.js
@@ -1,14 +1,16 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useMemo, Children } from 'react'
 import styled from 'styled-components'
 import Center from './Center'
 import Stack from './Stack'
 
 const Choices = ({ answer, setAnswer, type, correctAnswer, incorrectAnswers }) => {
-  // TODO: shuffle it
-  const [first, second, third, fourth] = [correctAnswer, ...incorrectAnswers]
+  const choices = [correctAnswer, ...incorrectAnswers]
+  // memoizing the array of choices to avoid re-ordering the
+  // choices at each re-render of the same question
+  const [first, second, third, fourth] = useMemo(() => shuffle(choices), [correctAnswer])
 
   return (
-    <Stack direction='v' height='30%' width='100%' disabled>
+    <Stack direction='v' height='30%' width='100%'>
       <Center margin='0 0 10px 0'>
         <Stack direction='h'>
           <ChoiceButton setAnswer={setAnswer} answer={answer} choice={first} correctAnswer={correctAnswer} />
@@ -24,6 +26,23 @@ const Choices = ({ answer, setAnswer, type, correctAnswer, incorrectAnswers }) =
         </Center>}
     </Stack>
   )
+
+  function shuffle (array) {
+    let currentIndex = array.length; let randomIndex
+
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+    // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]]
+    }
+
+    return array
+  }
 }
 
 export default Choices
@@ -33,7 +52,7 @@ const ChoiceButton = ({ setAnswer, answer, choice, correctAnswer }) => {
   const isCorrect = answer && choice === correctAnswer
 
   return (
-    <Button selected={selected} correct={isCorrect} disabled={answer} onClick={handleClick}>{choice}</Button>
+    <Button key={choice} selected={selected} correct={isCorrect} disabled={answer} onClick={handleClick}>{choice}</Button>
   )
   function handleClick () {
     setAnswer(choice)
