e740376777790475cd2ac886d2ef00689cd516c2
JLL32 <eabduljalil@outlook.com>
2022-04-27 06:00:05 +0000

    feat: add overlapping score bar (not finished)

--- src/components/ScoreBar.js
+++ src/components/ScoreBar.js
@@ -5,22 +5,68 @@ import Box from './Box'
 
 const ScoreBar = ({ score, minScore, maxScore }) => {
   return (
-    <Stack direction='v'>
-      <Stack direction='h' height='100%' justify='space-between' />
-      <Box className='progress'>
-        <Bar className='progress-bar' role='progressbar' value={minScore} aria-valuenow={minScore} aria-valuemin='0' aria-valuemax='100' />
+    <Box>
+    <TextContainer>
+      <Box>
+        score: {score}%
       </Box>
-      <Box className='progress'>
-        <Bar className='progress-bar bg-success' role='progressbar' value={score} aria-valuenow={score} aria-valuemin='0' aria-valuemax='100' />
+      <Box>
+        max score: {maxScore}%
       </Box>
-      <Box className='progress'>
-        <Bar className='progress-bar bg-info' role='progressbar' value={maxScore} aria-valuenow={maxScore} aria-valuemin='0' aria-valuemax='100' />
-      </Box>
-    </Stack>
+    </TextContainer>
+    <Parent>
+    <Bar3></Bar3>
+    <Bar2></Bar2>
+    <Bar1></Bar1>
+    <Bar4></Bar4>
+    </ Parent>
+    </Box>
   )
 }
 
-const Bar = styled.div`
-  width: ${props => props.value}%;
+// const Bar = styled.div`
+//   width: ${props => props.value}%;
+// `
+
+const Parent = styled.div`
+width: 100%;
+height: 100%;
+position: relative;
+border-radius: 8px;
 `
+
+const Bar1 = styled(Box)`
+background-color: gray;
+position: absolute;
+width: 50%;
+left: 0;
+top: 0;
+bottom: 0;
+right: 50px;
+border-radius: 8px 0 0 8px;
+height: 35%;
+`
+const Bar2 = styled(Bar1)`
+background-color: aliceblue;
+width: 80%;
+`
+
+const Bar3 = styled(Bar1)`
+background-color: moccasin;
+width: 90%;
+`
+
+const Bar4 = styled(Bar1)`
+background-color: transparent;
+border: 1px solid black;
+border-radius: 8px;
+width: 100%;
+`
+
+const TextContainer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+`
+
 export default ScoreBar
