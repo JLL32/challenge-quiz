8d5a98eb1825b2dab1bc166b0c0624e1f31a675a
JLL32 <eabduljalil@outlook.com>
2022-04-28 04:02:12 +0000

    feat: add styling to the progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
+import Box from './Box'
 
 const ProgressBar = ({ value }) => {
   return (
-    <div className='progress'>
-      <Bar className='progress-bar' role='progressbar' value={value} aria-valuenow={value} aria-valuemin='0' aria-valuemax='100' />
-    </div>
+    <BarContainer>
+      <Bar value={value}/>
+      <BorderBar/>
+    </BarContainer>
   )
 }
 
 export default ProgressBar
 
+const BarContainer = styled.div`
+width: 100%;
+display: grid;
+justify-items: start;
+align-items: start;
+`
 const Bar = styled.div`
+height: 20px;
+grid-column-start: 1;
+grid-row-start: 1;
 width: ${props => props.value}%;
+background-color: var(--teal);
+border-radius: var(--radius) 0 0 var(--radius);
+width: ${props => props.value}%;
+border-radius: ${props => props.value === 100 ? 'var(--radius)' : ''};
+`
+const BorderBar = styled(Bar)`
+background-color: transparent;
+border: 1px solid var(--teal);
+border-radius: var(--radius);
+width: 100%;
 `
