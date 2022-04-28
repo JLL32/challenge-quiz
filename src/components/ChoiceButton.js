e515e555123f574ac204dbff1eb3912fb118897d
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-04-28 17:17:31 +0000

    refactor: move ChoiceButton to its own component

---
+++ src/components/ChoiceButton.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ChoiceButton = ({ choose, answer, choice, correctAnswer }) => {
+  const selected = answer === choice
+  const isCorrect = answer && choice === correctAnswer
+
+  return (
+    <Button
+      key={choice} selected={selected} correct={isCorrect} disabled={answer}
+      onClick={handleClick}
+    >{choice}
+    </Button>
+  )
+  function handleClick () {
+    choose(choice)
+  }
+}
+
+export default ChoiceButton
+
+const Button = styled.button`
+width: 100%;
+background-color: white;
+padding: 8px;
+border: none;
+background-color: ${props => props.correct ? 'var(--teal)' : ''};
+background-color: ${props => props.selected && !props.correct ? '#BF3D44' : ''};
+color: ${props => (props.correct || props.selected) && 'white'};
+flex: 1;
+cursor: pointer;
+border-radius: var(--radius);
+`
--- src/components/Choices.js
+++ src/components/Choices.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import useChoices from '../hooks/useChoices'
+import ChoiceButton from './ChoiceButton'
 
 const Choices = ({ answer, choose, correctAnswer, incorrectAnswers }) => {
   const { choices: [first, second, third, fourth], count } =
@@ -9,44 +10,31 @@ const Choices = ({ answer, choose, correctAnswer, incorrectAnswers }) => {
   return (
     <ChoicesContainer>
       <BinaryContainer>
-        <ChoiceButton choose={choose} answer={answer} choice={first} correctAnswer={correctAnswer} />
-        <ChoiceButton choose={choose} answer={answer} choice={second} correctAnswer={correctAnswer} />
+        <ChoiceButton
+          choose={choose} answer={answer} choice={first}
+          correctAnswer={correctAnswer}
+        />
+        <ChoiceButton
+          choose={choose} answer={answer} choice={second}
+          correctAnswer={correctAnswer}
+        />
       </BinaryContainer>
       {(count === 4) &&
         <BinaryContainer>
-          <ChoiceButton choose={choose} answer={answer} choice={third} correctAnswer={correctAnswer} />
-          <ChoiceButton choose={choose} answer={answer} choice={fourth} correctAnswer={correctAnswer} />
+          <ChoiceButton
+            choose={choose} answer={answer} choice={third}
+            correctAnswer={correctAnswer}
+          />
+          <ChoiceButton
+            choose={choose} answer={answer} choice={fourth}
+            correctAnswer={correctAnswer}
+          />
         </BinaryContainer>}
     </ChoicesContainer>
   )
 }
 
 export default Choices
-
-const ChoiceButton = ({ choose, answer, choice, correctAnswer }) => {
-  const selected = answer === choice
-  const isCorrect = answer && choice === correctAnswer
-
-  return (
-    <Button key={choice} selected={selected} correct={isCorrect} disabled={answer} onClick={handleClick}>{choice}</Button>
-  )
-  function handleClick () {
-    choose(choice)
-  }
-}
-
-const Button = styled.button`
-width: 100%;
-background-color: white;
-padding: 8px;
-border: none;
-background-color: ${props => props.correct ? 'var(--teal)' : ''};
-background-color: ${props => props.selected && !props.correct ? '#BF3D44' : ''};
-color: ${props => (props.correct || props.selected) && 'white'};
-flex: 1;
-cursor: pointer;
-border-radius: var(--radius);
-`
 
 const ChoicesContainer = styled.div`
 width: 100%;
