d180b306a6e68a46c8ec136b5d5fbf1e9682f186
JLL32 <eabduljalil@outlook.com>
2022-04-28 20:10:52 +0000

    refactor: move primary button to its own file

---
+++ src/components/PrimaryButton.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const PrimaryButton = styled.button`
+background-color: var(--teal);
+color: #FFFFFF;
+border-radius: var(--radius);
+padding: 8px;
+border: none;
+cursor: pointer;
+width: 200px;
+`
+export default PrimaryButton
