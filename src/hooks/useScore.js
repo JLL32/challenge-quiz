688031c87b29f7a59bd43e8d0115fa3cac906d14
JLL32 <eabduljalil@outlook.com>
2022-04-29 00:54:26 +0000

    fix: fix rounding bug in score

--- src/hooks/useScore.js
+++ src/hooks/useScore.js
@@ -2,11 +2,12 @@ export default useScore
 
 function useScore (answeredQuestions, correctAnswers, count) {
   const score = answeredQuestions > 0
-    ? Math.floor(correctAnswers / answeredQuestions * 100)
+    ? Math.round(correctAnswers / answeredQuestions * 100)
     : 0
-  const maxScore = 100 - Math.floor(
+
+  const maxScore = 100 - Math.round(
     (answeredQuestions - correctAnswers) / count * 100)
-  const minScore = Math.floor(
+  const minScore = Math.round(
     maxScore - (count - answeredQuestions) / count * 100)
   return { score, maxScore, minScore }
 }
