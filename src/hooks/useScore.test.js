0ea38149012e196c93d1c5b05261ee5b171a79c0
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-05-02 04:41:55 +0000

    test: split useScore test into multiple ones

--- src/hooks/useScore.test.js
+++ src/hooks/useScore.test.js
@@ -2,15 +2,20 @@ import { renderHook } from '@testing-library/react-hooks'
 import useScore from './useScore'
 
 describe('useScore', () => {
-  it('score, max score, min score should be 67%, 75%, 50%', () => {
-    const answeredQuestions = 15
-    const correctAnswers = 10
-    const count = 20
-    const { result } = renderHook(
-      () => useScore(answeredQuestions, correctAnswers, count))
-    const { score, maxScore, minScore } = result.current
+  const answeredQuestions = 15
+  const correctAnswers = 10
+  const count = 20
+  const { result } = renderHook(
+    () => useScore(answeredQuestions, correctAnswers, count))
+  const { score, maxScore, minScore } = result.current
+
+  it('score should be 67%', () => {
     expect(score).toBe(67)
+  })
+  it('max score should be 75%', () => {
     expect(maxScore).toBe(75)
+  })
+  it('min score should be 50%', () => {
     expect(minScore).toBe(50)
   })
 })
