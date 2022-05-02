d9679c0df46cc6bd53d1dbeb57203e8c1b7e6f30
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-05-02 03:58:43 +0000

    fix: fix a bug in min score calculation

--- src/hooks/useScore.js
+++ src/hooks/useScore.js
@@ -7,7 +7,6 @@ function useScore (answeredQuestions, correctAnswers, count) {
 
   const maxScore = 100 - Math.round(
     (answeredQuestions - correctAnswers) / count * 100)
-  const minScore = Math.round(
-    maxScore - (count - answeredQuestions) / count * 100)
+  const minScore = Math.round(correctAnswers / count * 100)
   return { score, maxScore, minScore }
 }
