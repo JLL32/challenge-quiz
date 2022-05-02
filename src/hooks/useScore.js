2f95ab5bef21aedaf3ad22afdb38f3c9b8132d1f
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-05-02 06:27:04 +0000

    refactor: make zero division explicitly handled

--- src/hooks/useScore.js
+++ src/hooks/useScore.js
@@ -1,12 +1,18 @@
 export default useScore
 
 function useScore (answeredQuestions, correctAnswers, count) {
-  const score = answeredQuestions > 0
-    ? Math.round(correctAnswers / answeredQuestions * 100)
-    : 0
+  if (answeredQuestions === 0) {
+    return {
+      score: 0,
+      maxScore: 0,
+      minScore: 0
+    }
+  }
 
+  const score = Math.round(correctAnswers / answeredQuestions * 100)
   const maxScore = 100 - Math.round(
     (answeredQuestions - correctAnswers) / count * 100)
   const minScore = Math.round(correctAnswers / count * 100)
+
   return { score, maxScore, minScore }
 }
